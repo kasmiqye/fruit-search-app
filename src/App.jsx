035e259306eaf1c603aa0fc47a867720e7d1be5b
kasmiqye <poamrb@gmail.com>
2025-10-13 21:20:31 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FruitsSearch from "./FruitsSearch";
+
+function App() {
+  return (
+    <div>
+      <FruitsSearch />
+    </div>
+  );
+}
+
+export default App;
